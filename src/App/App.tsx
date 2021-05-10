d4422905d0d69cdf09fa48e1ec752746195a9e43
Ilya <idkehift@yandex.ru>
2021-05-10 17:20:55 +0300

    Form on Material UI

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,47 @@
-import React from 'react';
-import './App.css';
-import {TextField} from "@material-ui/core";
+import React from "react";
+import "./App.css";
+import { Button, makeStyles, Paper, TextField } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
+const useStyles = makeStyles((theme) => ({
+  Paper: {
+    width: 400,
+    margin: "20px auto",
+    justifyContent: "center",
+  },
+  Item: {
+    margin: 20,
+  },
+  Button: {
+    marginBottom: 20,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <form noValidate autoComplete="off">
-        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <Paper className={classes.Paper}>
+          <Typography variant="h4">TS Form Example</Typography>
+          <div className={classes.Item}>
+            <TextField id="standard-basic" label="A" fullWidth />
+          </div>
+          <div className={classes.Item}>
+            <TextField id="standard-basic" label="B" fullWidth />
+          </div>
+          <div className={classes.Item}>
+            <TextField id="standard-basic" label="C" fullWidth />
+          </div>
+          <div className={classes.Item}>
+            <TextField id="standard-basic" label="D" fullWidth />
+          </div>
+          <div className={classes.Item}>
+            <Button variant="contained" className={classes.Button}>
+              Default
+            </Button>
+          </div>
+        </Paper>
       </form>
     </div>
   );
