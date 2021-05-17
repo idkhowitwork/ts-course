1771f6b89f6634213b86ee67369f88465969c27d
Ilya <idkehift@yandex.ru>
2021-05-17 17:20:02 +0300

    Lesson2 refactoring & casting

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -18,26 +18,33 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const isArrayOfNumber = (arr: (number | string)[]): arr is number[] => {
+  return arr.every((value) => typeof value === 'number');
+};
+
 function App() {
   const classes = useStyles();
 
-  const [a, setA] = useState<string | number>('');
-  const [b, setB] = useState<string | number>('');
-  const [c, setC] = useState<string | number>('');
-  const [d, setD] = useState<string | number>('');
+  const [items, setItems] = useState<(string | number)[]>(['a', 'b', 123, 'd']);
 
-  const [all, setAll] = useState({ a, b, c, d });
+  const [all, setAll] = useState([...items]);
 
   const [sumStr, setSumStr] = useState('');
   const [sum, setSum] = useState(0);
 
-  const handleSumbit = () => {
-    setAll({ a, b, c, d });
-    if (typeof a === 'number' && typeof b === 'number' && typeof c === 'number' && typeof d === 'number') {
-      setSum(a + b + c + d);
+  const handleSubmit = () => {
+    setAll([...items]);
+    if (isArrayOfNumber(items)) {
+      setSum(items.reduce((acc, value) => acc + value, 0));
     } else {
-      setSumStr(`${a}${b}${c}${d}`);
+      setSumStr(items.reduce((acc: string, value) => acc + `${value}`, ''));
     }
+  };
+
+  const handleChange = (value: number | string, index: number) => {
+    const itms = [...items];
+    itms[index] = value;
+    setItems(itms);
   };
 
   return (
@@ -47,47 +54,23 @@ function App() {
         noValidate
         onSubmit={(event) => {
           event.preventDefault();
-          handleSumbit();
+          handleSubmit();
         }}
       >
         <Paper className={classes.Paper}>
           <Typography variant="h4">TS Form Example</Typography>
-          <div className={classes.Item}>
-            <TextField
-              fullWidth
-              id="standard-basic"
-              label="A"
-              value={a}
-              onChange={(e) => setA(isNaN(+e.target.value) ? e.target.value : +e.target.value)}
-            />
-          </div>
-          <div className={classes.Item}>
-            <TextField
-              fullWidth
-              id="standard-basic"
-              label="B"
-              value={b}
-              onChange={(e) => setB(isNaN(+e.target.value) ? e.target.value : +e.target.value)}
-            />
-          </div>
-          <div className={classes.Item}>
-            <TextField
-              fullWidth
-              id="standard-basic"
-              label="C"
-              value={c}
-              onChange={(e) => setC(isNaN(+e.target.value) ? e.target.value : +e.target.value)}
-            />
-          </div>
-          <div className={classes.Item}>
-            <TextField
-              fullWidth
-              id="standard-basic"
-              label="D"
-              value={d}
-              onChange={(e) => setD(isNaN(+e.target.value) ? e.target.value : +e.target.value)}
-            />
-          </div>
+          {items.map((item, index) => (
+            <div className={classes.Item} key={index}>
+              <TextField
+                fullWidth
+                id="standard-basic"
+                label={`${index}`}
+                value={item}
+                onChange={(e) => handleChange(isNaN(+e.target.value) ? e.target.value : +e.target.value, index)}
+              />
+            </div>
+          ))}
+
           <div className={classes.Item}>
             <Button className={classes.Button} type="submit" variant="contained">
               Submit
