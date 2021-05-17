30ae9c1e29372e8536de38b21d5a4cf4626b23eb
Ilya <idkehift@yandex.ru>
2021-05-17 15:35:23 +0300

    Lesson2 Sums

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -21,41 +21,72 @@ const useStyles = makeStyles((theme) => ({
 function App() {
   const classes = useStyles();
 
-  const [a, setA] = useState('');
-  const [b, setB] = useState('');
-  const [c, setC] = useState('');
-  const [d, setD] = useState('');
+  const [a, setA] = useState<string | number>('');
+  const [b, setB] = useState<string | number>('');
+  const [c, setC] = useState<string | number>('');
+  const [d, setD] = useState<string | number>('');
 
-  const [all, setAll] = useState({
-    a,
-    b,
-    c,
-    d,
-  });
+  const [all, setAll] = useState({ a, b, c, d });
+
+  const [sumStr, setSumStr] = useState('');
+  const [sum, setSum] = useState(0);
+
+  const handleSumbit = () => {
+    setAll({ a, b, c, d });
+    if (typeof a === 'number' && typeof b === 'number' && typeof c === 'number' && typeof d === 'number') {
+      setSum(a + b + c + d);
+    } else {
+      setSumStr(`${a}${b}${c}${d}`);
+    }
+  };
 
   return (
     <div className="App">
       <form
         autoComplete="off"
         noValidate
-        onSubmit={(e) => {
-          e.preventDefault();
-          setAll({ a, b, c, d });
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleSumbit();
         }}
       >
         <Paper className={classes.Paper}>
           <Typography variant="h4">TS Form Example</Typography>
           <div className={classes.Item}>
-            <TextField fullWidth id="standard-basic" label="A" value={a} onChange={(e) => setA(e.target.value)} />
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="A"
+              value={a}
+              onChange={(e) => setA(isNaN(+e.target.value) ? e.target.value : +e.target.value)}
+            />
           </div>
           <div className={classes.Item}>
-            <TextField fullWidth id="standard-basic" label="B" value={b} onChange={(e) => setB(e.target.value)} />
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="B"
+              value={b}
+              onChange={(e) => setB(isNaN(+e.target.value) ? e.target.value : +e.target.value)}
+            />
           </div>
           <div className={classes.Item}>
-            <TextField fullWidth id="standard-basic" label="C" value={c} onChange={(e) => setC(e.target.value)} />
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="C"
+              value={c}
+              onChange={(e) => setC(isNaN(+e.target.value) ? e.target.value : +e.target.value)}
+            />
           </div>
           <div className={classes.Item}>
-            <TextField fullWidth id="standard-basic" label="D" value={d} onChange={(e) => setD(e.target.value)} />
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="D"
+              value={d}
+              onChange={(e) => setD(isNaN(+e.target.value) ? e.target.value : +e.target.value)}
+            />
           </div>
           <div className={classes.Item}>
             <Button className={classes.Button} type="submit" variant="contained">
@@ -66,6 +97,10 @@ function App() {
       </form>
       <Paper className={classes.Paper}>
         <pre>{JSON.stringify(all, null, 2)}</pre>
+        <hr />
+        {sumStr}
+        <hr />
+        {sum}
       </Paper>
     </div>
   );
